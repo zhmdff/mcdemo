d1235617aa1aa89f9627a062f8660d24cf1a71bc
zhmdff <mahmud.ehmed06@gmail.com>
2024-02-15 21:51:02 +0400

    first commit

---
+++ mineflayer.js
@@ -0,0 +1,20 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'biriyirmiiki.aternos.me', // minecraft server ip
+  username: 'ramos', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
+  auth: 'offline',
+  port: 18911,
+})
+
+bot.on('chat', (username, message) => {
+  if (username === bot.username) return
+  bot.chat(message)
+})
+
+async function runRconCommand(user_text) {
+    bot.chat(user_text)
+}
+
+// Export the function to make it accessible from other files
+module.exports = { runRconCommand };
